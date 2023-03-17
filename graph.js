f047d2067013ccc58899508ca7c9c09a7018cdeb
mariered0 <marieakg@gmail.com>
2023-03-16 21:28:00 -0700

    complete

--- graph.js
+++ graph.js
@@ -16,19 +16,43 @@ class Graph {
   }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for(let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    this.nodes.delete(vertex);
+    for(let node of this.nodes){
+      if(node.adjacent.has(vertex)) node.adjacent.delete(vertex);
+    }
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    let toVisitStack = [start];
+    let seen = [];
+    console.log('toVisitStack:', toVisitStack);
+    while(toVisitStack.length){
+      let currNode = toVisitStack.pop();
+      toVisitStack.push();
+        seen.push(node.value);
+    }
+  }
 
   // this function returns an array of Node values using BFS
   breadthFirstSearch(start) {}
